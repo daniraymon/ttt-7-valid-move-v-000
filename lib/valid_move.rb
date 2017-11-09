81dc9dabb8e41e6bca826ebd69a8b83c3712cf27
Daniel Raymon <daniraymon@gmail.com>
2017-11-09 20:47:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
-def valid_move?(index)
-
+def valid_move?(ind)
+  if !position_taken?(ind) && [0,1,2,3,4,5,6,7,8].include?(ind)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
